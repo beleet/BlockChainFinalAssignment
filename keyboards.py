3287f41a526865878d1108d02a093fe60d9f611d
philipp3388 <71440514+philipp3388@users.noreply.github.com>
2023-12-03 22:08:32 +0300

    useless files deleted

--- keyboards.py
+++ keyboards.py
@@ -13,14 +13,7 @@ select_role_keyboard = ReplyKeyboardMarkup(
 
 author_main_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        # Chanel routs
         [KeyboardButton(text='/add_channel')],
-        # [KeyboardButton(text='/remove_channel')],
-        # [KeyboardButton(text='/list_subscribers')],
-        # # Users
-        # [KeyboardButton(text='/ban_subscriber')],
-        # [KeyboardButton(text='/unban_subscriber')],
-        # # Money
         # [KeyboardButton(text='/check_balance')],
         # [KeyboardButton(text='/withdraw')],
         [KeyboardButton(text='/change_role')],
@@ -33,8 +26,6 @@ subscriber_main_keyboard = ReplyKeyboardMarkup(
         # Chanel routs
         [KeyboardButton(text='/list_channels')],
         # [KeyboardButton(text='/subscribe')],
-        # [KeyboardButton(text='/unsubscribe')],
-        #
         # [KeyboardButton(text='/deposit')],
         [KeyboardButton(text='/change_role')],
     ],
@@ -48,32 +39,3 @@ admin_main_keyboard = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True,
 )
-
-
-def generate_list_of_channels(page: int = 0):
-
-    keyboard_builder = InlineKeyboardBuilder()
-
-    for _ in range(page, page + 5):
-        subscribe_button = InlineKeyboardButton(
-            text=database.channels[_],
-            url='http://aboba.com',
-        )
-
-        keyboard_builder.row(subscribe_button)
-
-    next_button = InlineKeyboardButton(
-        text='Next page >>>',
-        url='http://aboba.com'
-    )
-
-    previous_button = InlineKeyboardButton(
-        text='Previous page <<<',
-        url='http://aboba.com',
-    )
-
-    keyboard_builder.row(next_button)
-    keyboard_builder.row(previous_button)
-
-    return keyboard_builder.as_markup()
-
